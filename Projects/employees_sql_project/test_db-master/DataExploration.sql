eb83ab1ef9ee1bec0663e65e365613c138364670
saeed <77902443+saeedshiranii@users.noreply.github.com>
2023-12-17 22:07:10 +0330

    all subqueries (except last one ) are done.

--- Projects/employees_sql_project/test_db-master/DataExploration.sql
+++ Projects/employees_sql_project/test_db-master/DataExploration.sql
@@ -499,3 +499,100 @@ ORDER BY salary DESC;
 
 
 # How many male and how many female managers do we have in the employees database?
+
+SELECT 
+    gender, COUNT(gender)
+FROM
+    dept_manager dm
+        JOIN
+    employees e ON dm.emp_no = e.emp_no
+GROUP BY (gender);
+
+
+/*
+Extract the information about all department managers 
+who were hired between the 1stof January 1990 and the 1stof January 1995.*/
+select * from dept_manager limit 10 ;
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+            
+            
+            
+/*Select the entire information for all employees whose job title is Assistant Engineer.
+
+Hint: To solve this exercise, use the 'employees' table.*/
+  
+SELECT 
+    *
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            titles t
+        WHERE
+            t.emp_no = e.emp_no
+                AND title = 'Assistant Engineer');
+    
+    
+    
+    
+    
+/*Create the emp_manager table, using the following code:
+*/
+
+
+DROP TABLE IF EXISTS emp_manager;
+
+CREATE TABLE emp_manager (
+
+emp_no INT(11) NOT NULL,
+
+dept_no CHAR(4) NULL,
+
+manager_no INT(11) NOT NULL
+
+);
+
+/*
+
+Fill emp_manager with data about employees, the number of the department they are working in,
+ and their managers.  Your query skeleton must be:
+Insert INTO emp_manager SELECT
+U.*
+FROM
+(A)
+UNION (B) UNION (C) UNION (D) AS U;
+A and B should be the same subsets used in the last lecture(SQL Subqueries Nested in SELECT and FROM).
+ In other words,
+ assign employee number 110022 as a manager to all employees from 10001 to 10020 (this must be subset A), 
+ and employee number 110039 as a manager to all employees from 10021 to 10040 (this must be subset B).
+Use the structure of subset A to create subset C,
+ where you must assign employee number 110039 as a manager to employee 110022.
+ emp_manager
+Following the same logic, create subset D.
+ Here you must do the opposite - assign employee 110022 as a manager to employee 110039.
+Your output must contain 42 rows.
+Good luck!
+*/
+Insert INTO emp_manager SELECT
+U.*
+FROM
+(A)
+UNION (B) UNION (C) UNION (D) AS U;
+
+
+
+
+
+
